9557002e9c1a7596255ef6e25b39667445a0cf4d
aymen.chayeb <aymen.chayeb.ing@gmail.com>
2025-02-19 11:30:58 +0100

    implement the create account controller

--- src/main/java/com/example/bank_api/model/Account.java
+++ src/main/java/com/example/bank_api/model/Account.java
@@ -1,12 +1,22 @@
 package com.example.bank_api.model;
 
-import lombok.AllArgsConstructor;
+import jakarta.persistence.*;
 import lombok.Data;
 
+@Entity
+@Table(name = "accounts")
 @Data
-@AllArgsConstructor
 public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String accountId;
     private String owner;
     private double balance;
+
+    public Account(String accountId, String owner, double balance) {
+        this.accountId = accountId;
+        this.owner = owner;
+        this.balance = balance;
+    }
 }
--- src/test/java/com/example/bank_api/controller/AccountControllerTests.java
+++ src/test/java/com/example/bank_api/controller/AccountControllerTests.java
@@ -1,19 +1,23 @@
 package com.example.bank_api.controller;
 
-import com.example.bank_api.model.Account;
 import com.example.bank_api.service.AccountService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(AccountController.class)
+@ExtendWith(MockitoExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class AccountControllerTests {
 
     @Autowired
@@ -22,18 +26,18 @@ public class AccountControllerTests {
     @Mock
     private AccountService accountService;
 
+    @InjectMocks
+    private AccountController accountController;
+
     @Test
     public void testCreateAccount() throws Exception {
-        String accountId = "1";
-        String owner = "CHAYEB";
-        double balance = 0;
 
-        Account expectedAccount = new Account(accountId, owner, balance);
-        when(accountService.createAccount(anyString())).thenReturn(expectedAccount);
-
-        mockMvc.perform(post("/api/accounts")
-                        .param("owner", owner))
-                .andExpect(status().isCreated());
+        mockMvc.perform(post("/bank_api/accounts")
+                        .param("owner", "CHAYEB"))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.owner").value("CHAYEB"))
+                .andExpect(jsonPath("$.balance").value(0));
+        ;
     }
 
 
