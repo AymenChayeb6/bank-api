2c8a7b849ba6a328de09f79f90c11161cb9468cf
aymen.chayeb <aymen.chayeb.ing@gmail.com>
2025-02-19 19:46:55 +0100

    Add swagger configuration

--- src/main/java/com/example/bank_api/controller/AccountController.java
+++ src/main/java/com/example/bank_api/controller/AccountController.java
@@ -4,6 +4,8 @@ import com.example.bank_api.exception.AccountNotFoundException;
 import com.example.bank_api.exception.InsufficientBalanceException;
 import com.example.bank_api.model.Account;
 import com.example.bank_api.service.AccountService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/bank_api/accounts")
+@Tag(name = "AccountController", description = "API for managing bank accounts")
 public class AccountController {
 
     private final AccountService accountService;
@@ -23,12 +26,14 @@ public class AccountController {
         this.accountService = accountService;
     }
 
+    @Operation(summary = "create a new account")
     @PostMapping
     public ResponseEntity<Account> createAccount(@RequestParam String owner) {
         Account createdAccount = accountService.createAccount(owner);
         return new ResponseEntity<>(createdAccount, HttpStatus.CREATED);
     }
 
+    @Operation(summary = "deposit money into an account")
     @PostMapping("/deposit")
     public ResponseEntity<?> depositMoney(@RequestParam String accountId, @RequestParam double amount) {
         try {
@@ -39,6 +44,7 @@ public class AccountController {
         }
     }
 
+    @Operation(summary = "withdraw money from an account")
     @PostMapping("/withdraw")
     public ResponseEntity<?> withdrawMoney(@RequestParam String accountId, @RequestParam double amount) {
         try {
